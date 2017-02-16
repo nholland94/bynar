d06a0bcd3531e08d6e36564f5a63601938782911
Nathan Holland <nholland94@gmail.com>
2017-02-16 01:16:11 -0700

    Build validation layers on travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,6 +7,19 @@ make
 cd -
 export PATH="$PATH:$HOME/spirv-tools/build/tools"
 
+# -- Build loader and validation layers
+
+wd="$(pwd)"
+cd ~/Vulkan-LoaderAndValidationLayers
+./update_external_sources.sh
+cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug
+cd build
+make
+cd "${wd}"
+
+export LD_LIBRARY_PATH="$HOME/Vulkan-LoaderAndValidationLayers/build/loaders:$LD_LIBRARY_PATH"
+export VK_LAYER_PATH="$HOME/Vulkan-LoaderAndValidationLayers/build/layers"
+
 # -- Run tests
 
 dub test --compiler=${DC}
